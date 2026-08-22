53633030c92d74cccf9ee6597ccf83d873b18590
Ashmeet Singh <229201319+Ashmeet408@users.noreply.github.com>
2026-08-22 17:36:50 -0500

    Add common password blocklist with case insensitive matching

--- checker.py
+++ checker.py
@@ -1,10 +1,21 @@
 stored = []
+common_passwords = ["correcthorsebatterystaple", 
+                    "123456789012345", 
+                    "passwordpassword", 
+                    "qwertyuiopasdfgh", 
+                    "iloveyouiloveyou", 
+                    "letmeinletmein12", 
+                    "adminadminadmin1",
+                    "password123"]
+
 password = input("Enter Password: ")
 
 if len(password) < 15:
     stored.append("Password is too short. Password must be at least 15 characters long.")
 if len(password) >= 65:
     stored.append("Password is too long. Password must be at most 64 characters long.")
+if password.lower() in common_passwords:
+    stored.append("Password is a common password. Please try again.")
 
 if len(stored) == 0:
     print("PASS")
